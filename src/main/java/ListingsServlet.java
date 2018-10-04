7f5a515e2bb7a87068ef6dfe8e1d7408dd92a82c
Max <maxim.zhabinets@nu.edu.kz>
2018-10-04 17:42:00 +0600

    commit

---
+++ src/main/java/ListingsServlet.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.google.gson.Gson;
+
+@Path("/listings")
+public class ListingsServlet {
+
+    @GET
+    public Response getListings() {
+
+    }
+}
