dfdf82743cd7c008b9f2e9590b2dafcb280a3aa8
Max <maxim.zhabinets@nu.edu.kz>
2018-10-24 14:40:12 +0600

    Changed Listing fields

--- src/main/java/Listing.java
+++ src/main/java/Listing.java
@@ -1,29 +1,36 @@
 
 
 public class Listing {
-    String username;
+    String email;
     String title;
+    String city;
+    String building;
+    int num_of_rooms;
     String description;
     int price;
-    String address;
     String postdate;
+    String contact_info;
 
 
-    public Listing(String username, String title, String description, int price, String address, String postdate){
-        this.username = username;
+
+    public Listing(String email, String title, String city, String building, int num_of_rooms, String description, int price, String postdate, String contact_info){
+        this.email = email;
         this.title = title;
+        this.city = city;
+        this.building = building;
         this.description = description;
         this.price = price;
-        this.address = address;
+        this.contact_info = contact_info;
         this.postdate = postdate;
+        this.num_of_rooms = num_of_rooms;
     }
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getTitle() {
@@ -32,6 +39,30 @@ public class Listing {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public int getNum_of_rooms() {
+        return num_of_rooms;
+    }
+
+    public void setNum_of_rooms(int num_of_rooms) {
+        this.num_of_rooms = num_of_rooms;
     }
 
     public String getDescription() {
@@ -50,19 +81,19 @@ public class Listing {
         this.price = price;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public String getPostdate() {
         return postdate;
     }
 
     public void setPostdate(String postdate) {
         this.postdate = postdate;
+    }
+
+    public String getContact_info() {
+        return contact_info;
+    }
+
+    public void setContact_info(String contact_info) {
+        this.contact_info = contact_info;
     }
 }
