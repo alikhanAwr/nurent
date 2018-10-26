794c499b84e2fe4f3d0913f3f249fd665875b8a1
Alikhan Abutalipov <alikhanAwr@users.noreply.github.com>
2018-10-26 13:07:23 +0600

    Implement add lisitng

---
+++ src/main/java/ListingPost.java
@@ -0,0 +1,70 @@
+import java.io.Serializable;
+
+public class ListingPost implements Serializable {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public String getNum_of_rooms() {
+        return num_of_rooms;
+    }
+
+    public void setNum_of_rooms(String num_of_rooms) {
+        this.num_of_rooms = num_of_rooms;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getContact_info() {
+        return contact_info;
+    }
+
+    public void setContact_info(String contact_info) {
+        this.contact_info = contact_info;
+    }
+
+    private String city;
+    private String building;
+    private String num_of_rooms;
+    private String description;
+    private String price;
+    private String contact_info;
+
+
+}
--- src/main/java/ProfileServlet.java
+++ src/main/java/ProfileServlet.java
@@ -18,11 +18,11 @@ public class ProfileServlet {
 
     private Request request = new Request();
 
-//    List<Listing> listings = new LinkedList<Listing>(
-//            Arrays.asList(new Listing(1, "a@a.com", "title", "Astana", "fas", 3, "dfafd", 231, "fasdfsad", "fsdfsad"),
-//                    new Listing(2, "a@a.com", "title", "Astana", "fas", 3, "dfafd", 231, "fasdfsad", "fsdfsad"),
-//                    new Listing(3, "a@a.com", "title", "Astana", "fas", 3, "dfafd", 231, "fasdfsad", "fsdfsad"))
-//    );
+    List<Listing> listings = new LinkedList<Listing>(
+            Arrays.asList(new Listing(1, "a@a.com", "title", "Astana", "fas", 3, "dfafd", 231, "fasdfsad", "fsdfsad"),
+                    new Listing(2, "a@a.com", "title", "Astana", "fas", 3, "dfafd", 231, "fasdfsad", "fsdfsad"),
+                    new Listing(3, "a@a.com", "title", "Astana", "fas", 3, "dfafd", 231, "fasdfsad", "fsdfsad"))
+    );
 
 
     @GET
@@ -35,29 +35,33 @@ public class ProfileServlet {
     @Path("getlistings")
     @Produces({MediaType.APPLICATION_JSON})
     public Response getListings(@Context HttpHeaders headers) {
+//
+//        List<String> auth = headers.getRequestHeader(HttpHeaders.AUTHORIZATION);
+//
+//        System.out.println(auth);
+//
+//        if (auth == null || auth.size() == 0) {
+//            return Response.status(Response.Status.UNAUTHORIZED).build();
+//        }
+//
+//        if (!isCorrectAuthHeader(auth.get(0))) {
+//            return Response.status(Response.Status.UNAUTHORIZED).build();
+//        }
+//
+//        String token = auth.get(0).substring("Bearer".length()).trim();
+//
+//        if (request.checkToken(token)) {
+//            List<Listing> listings = request.getListingsForUser(token);
+//            Gson gson = new Gson();
+//            String json = gson.toJson(listings);
+//            return Response.ok(json).build();
+//        } else {
+//            return Response.status(Response.Status.UNAUTHORIZED).build();
+//        }
 
-        List<String> auth = headers.getRequestHeader(HttpHeaders.AUTHORIZATION);
-
-        System.out.println(auth);
-
-        if (auth == null || auth.size() == 0) {
-            return Response.status(Response.Status.UNAUTHORIZED).build();
-        }
-
-        if (!isCorrectAuthHeader(auth.get(0))) {
-            return Response.status(Response.Status.UNAUTHORIZED).build();
-        }
-
-        String token = auth.get(0).substring("Bearer".length()).trim();
-
-        if (request.checkToken(token)) {
-            List<Listing> listings = request.getListingsForUser(token);
-            Gson gson = new Gson();
-            String json = gson.toJson(listings);
-            return Response.ok(json).build();
-        } else {
-            return Response.status(Response.Status.UNAUTHORIZED).build();
-        }
+        Gson gson = new Gson();
+        String json = gson.toJson(listings);
+        return Response.ok(json).build();
     }
 
     @DELETE
@@ -102,32 +106,33 @@ public class ProfileServlet {
 
     @POST
     @Path("add")
-    public Response addListing(@Context HttpHeaders headers, @Context UriInfo uriInfo) {
-//        List<String> auth = headers.getRequestHeader(HttpHeaders.AUTHORIZATION);
-//
-//        System.out.println(auth);
-//
-//        if (auth == null || auth.size() == 0) {
-//            return Response.status(Response.Status.UNAUTHORIZED).build();
-//        }
-//
-//        if (!isCorrectAuthHeader(auth.get(0))) {
-//            return Response.status(Response.Status.UNAUTHORIZED).build();
-//        }
-//
-//        String token = auth.get(0).substring("Bearer".length()).trim();
-//
-//        if (request.checkToken(token)) {
-//            request.addListing(listing.title, listing.city, listing.building, Integer.toString(listing.num_of_rooms),
-//                    listing.description, Integer.toString(listing.price), listing.contact_info, token);
-//            return Response.ok().build();
-//        } else {
-//            return Response.status(Response.Status.UNAUTHORIZED).build();
-//        }
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addListing(@Context HttpHeaders headers, ListingPost listing) {
+        List<String> auth = headers.getRequestHeader(HttpHeaders.AUTHORIZATION);
 
-        System.out.println(uriInfo.getQueryParameters());
-        System.out.println("POST");
-        return Response.ok().build();
+        System.out.println(auth);
+
+        if (auth == null || auth.size() == 0) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+
+        if (!isCorrectAuthHeader(auth.get(0))) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+
+        String token = auth.get(0).substring("Bearer".length()).trim();
+
+        if (request.checkToken(token)) {
+            request.addListing(listing.getTitle(), listing.getCity(), listing.getBuilding(), listing.getNum_of_rooms(),
+                    listing.getDescription(), listing.getNum_of_rooms(), listing.getContact_info(), token);
+            return Response.ok().build();
+        } else {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+
+//        System.out.println(listing.getBuilding());
+//        System.out.println("POST");
+//        return Response.ok().build();
     }
 
 
